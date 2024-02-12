92c79c76919538ef74dd0aaa05031bed4f6e3efe
amalbino0 <95870539+amalbino0@users.noreply.github.com>
2024-02-12 21:35:07 +0530

    polymorphism Overriding

---
+++ polymorphism/src/main/java/polym/OverA.java
@@ -0,0 +1,9 @@
+package polym;
+
+public class OverA {
+	public void display()
+	{
+		System.out.println("hai amal");
+	}
+
+}
---
+++ polymorphism/src/main/java/polym/OverB.java
@@ -0,0 +1,19 @@
+package polym;
+
+public class OverB  extends OverA{
+	public void display() {
+		super.display();
+
+			System.out.println("hello");
+	}
+
+	public static void main(String[] args) {
+		OverB obj=new OverB();
+		obj.display();
+		
+		
+		
+
+	}
+
+}
